290bcaf661439bad2d15e00bade4f3fddfb8f523
samAricha <samaricha001@mail.com>
2022-02-21 23:05:04 +0300

    password reset

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
